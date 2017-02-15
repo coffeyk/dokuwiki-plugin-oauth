d4db0d658541f0d6a2aadc96ae0b007ebfc07951
Kevin Coffey <kevin.coffey@datadoghq.com>
2017-02-15 11:38:49 -0500

    try messages

--- classes/DiscordAdapter.php
+++ classes/DiscordAdapter.php
@@ -33,7 +33,7 @@ class DiscordAdapter extends AbstractAdapter {
         /** var OAuth\OAuth2\Service\Generic $this->oAuth */
         $guild_id = 225596062302208000;
         $guild_user_result = $JSON->decode($this->oAuth->request('https://discordapp.com/api/guilds/'.$guild_id.'/members/@me'));
-        throw new Exception($guild_user_result);
+        msg($guild_user_result);
         $data['user'] = 'discord-'.$guild_user_result['user']['id'];
         if (count($guild_user_result['nick']) > 0) {
             $data['name'] = $guild_user_result['nick'];
