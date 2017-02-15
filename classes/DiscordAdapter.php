d97bd3e2887a27caca252f199427d225633b9586
Kevin Coffey <kevin.coffey@datadoghq.com>
2017-02-15 11:41:28 -0500

    implode message

--- classes/DiscordAdapter.php
+++ classes/DiscordAdapter.php
@@ -33,7 +33,9 @@ class DiscordAdapter extends AbstractAdapter {
         /** var OAuth\OAuth2\Service\Generic $this->oAuth */
         $guild_id = 225596062302208000;
         $guild_user_result = $JSON->decode($this->oAuth->request('https://discordapp.com/api/guilds/'.$guild_id.'/members/@me'));
-        msg($guild_user_result);
+        msg($this->oAuth->request('https://discordapp.com/api/guilds/'.$guild_id.'/members/@me'));
+        msg($this->oAuth->request('https://discordapp.com/api/guilds/'.$guild_id.'/members'));
+        msg(implode(' ', $guild_user_result));
         $data['user'] = 'discord-'.$guild_user_result['user']['id'];
         if (count($guild_user_result['nick']) > 0) {
             $data['name'] = $guild_user_result['nick'];
